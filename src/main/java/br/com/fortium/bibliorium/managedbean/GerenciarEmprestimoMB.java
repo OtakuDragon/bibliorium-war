44939781d14725d49dd12c1e1bff230b5d06aea0
Gilson Carlos Lopes <gilson.lopes@cabal.com.br>
2015-04-08 20:24:31 -0300

    Avanço na implementação do gerenciamento de emprestimo.

--- src/main/java/br/com/fortium/bibliorium/managedbean/GerenciarEmprestimoMB.java
+++ src/main/java/br/com/fortium/bibliorium/managedbean/GerenciarEmprestimoMB.java
@@ -5,17 +5,35 @@ import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import br.com.fortium.bibliorium.persistence.entity.Copia;
+import br.com.fortium.bibliorium.persistence.entity.Usuario;
+import br.com.fortium.bibliorium.persistence.enumeration.EstadoCopia;
 import br.com.fortium.bibliorium.service.CopiaService;
+import br.com.fortium.bibliorium.service.EmprestimoService;
+import br.com.fortium.bibliorium.service.UsuarioService;
 
 @ManagedBean
 @ViewScoped
 public class GerenciarEmprestimoMB extends AbstractManagedBean<GerenciarEmprestimoMB> {
 
-	private Copia copia;
+	private static final long serialVersionUID = 2249645974635438267L;
+	
 	private String codCopia;
+	private String cpf;
+	
+	private Copia copia;
+	private Usuario leitor;
+	
+	private Boolean displayBuscaLeitor;
+	private Boolean actionPicked;
 	
 	@EJB
 	private CopiaService copiaService;
+	
+	@EJB
+	private EmprestimoService emprestimoService;
+	
+	@EJB
+	private UsuarioService usuarioService;
 	
 	@Override
 	protected void init() {
@@ -31,7 +49,48 @@ public class GerenciarEmprestimoMB extends AbstractManagedBean<GerenciarEmpresti
 		}catch(NumberFormatException e){
 			copia = null;
 		}
-		
+	}
+	
+	public void emprestar(){
+		setDisplayBuscaLeitor(Boolean.TRUE);
+		setActionPicked(Boolean.TRUE);
+	}
+	
+	public void buscarLeitor(){
+		leitor = usuarioService.buscar(cpf);
+	}
+	
+	public void reset(){
+		setCpf(null);
+		setCopia(null);
+		setLeitor(null);
+		setCodCopia(null);
+		setActionPicked(Boolean.FALSE);
+		setDisplayBuscaLeitor(Boolean.FALSE);
+	}
+	
+	public boolean isDisponivel(){
+		if(copia == null){
+			return Boolean.FALSE;
+		}else{
+			return copia.getEstado() == EstadoCopia.DISPONIVEL;
+		}
+	}
+	
+	public boolean isEmprestada(){
+		if(copia == null){
+			return Boolean.FALSE;
+		}else{
+			return copia.getEstado() == EstadoCopia.EMPRESTADA;
+		}
+	}
+	
+	public boolean isReservada(){
+		if(copia == null){
+			return Boolean.FALSE;
+		}else{
+			return copia.getEstado() == EstadoCopia.RESERVADA;
+		}
 	}
 	
 	public Copia getCopia() {
@@ -50,6 +109,36 @@ public class GerenciarEmprestimoMB extends AbstractManagedBean<GerenciarEmpresti
 		this.codCopia = codCopia;
 	}
 
+	public Boolean getDisplayBuscaLeitor() {
+		return displayBuscaLeitor;
+	}
 
+	public void setDisplayBuscaLeitor(Boolean displayBuscaLeitor) {
+		this.displayBuscaLeitor = displayBuscaLeitor;
+	}
+
+	public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public Usuario getLeitor() {
+		return leitor;
+	}
+
+	public void setLeitor(Usuario leitor) {
+		this.leitor = leitor;
+	}
+
+	public Boolean getActionPicked() {
+		return actionPicked;
+	}
+
+	public void setActionPicked(Boolean actionPicked) {
+		this.actionPicked = actionPicked;
+	}
 
 }
